1d7177580e87198479dd3305199dea4c2e4bbf3d
Dipak kumar kewat <110630217+Dipakk10@users.noreply.github.com>
2022-11-07 23:18:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var names=new Array();
-names[0]="Yaakov";
+names[0]="intaz";
 names[1]="John";
-names[2]="Jen";
+names[2]="paula";
 names[3]="jason";
-names[4]="paul";
+names[4]="patlu";
 names[5]="frank";
 names[6]="larry";
-names[7]="paula";
+names[7]="Jan";
 names[8]="laura";
 names[9]="jim";
 
